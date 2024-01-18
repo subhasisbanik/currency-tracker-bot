3658c3fbd586eb31f161f6a7ab413cd30ce5e373
Subhasis <25420904+subhasisbanik@users.noreply.github.com>
2024-01-18 22:17:44 +0100

    initial code dump

---
+++ index.js
@@ -0,0 +1,78 @@
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('config');
+const axios = require('axios');
+const winston = require('winston');
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const selfChatId = config.get('Application.envConfig.telegram_bot.chat_id_self');
+const wiseAccessToken = process.env.WISE_ACCESS_TOKEN;
+
+const logger = winston.createLogger({
+  transports: [
+      new winston.transports.File({ filename: 'logs/app.log' }),
+      new winston.transports.Console()
+  ]
+});
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+let targetRate;
+
+bot.onText(/Hello/, (msg, match) => {
+  const chatId = msg.chat.id;
+  logger.info('Sending welcome message to sender..');
+  bot.sendMessage(chatId, 'Welcome to Currency Tracker. Please set your desired target rate with the command /targetrate <> to start tracking EUR to INR rates');
+});
+
+bot.onText(/\/targetrate (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    targetRate = match[1];
+    logger.info('Target rate set by sender..');
+    bot.sendMessage(chatId, 'Target rate of ', targetRate, "is set");
+});
+
+async function processSendMessage() {
+  if (null != targetRate){
+    logger.info("Target Rate set");
+    let exchangeRateVal = await fetchExchangeRate();
+    if(exchangeRateVal >= targetRate){
+      logger.info('Target rate met. Invoking message...')
+      bot.sendMessage(selfChatId, `The latest rate is : ${exchangeRateVal}`);
+    }
+  }
+}
+
+
+async function fetchExchangeRate() {
+  try {
+    //TODO: change this to adopt to multiple pairs of currency rates for every user
+
+    let wiseUrl = config.get('Application.envConfig.wise.url');
+    const response = await axios.get(wiseUrl, {
+      headers: { Authorization: `Bearer ${wiseAccessToken}` }
+  });
+    const exchangeRateObj = response.data;
+
+    if (null != exchangeRateObj) {
+      let exchangeRate = exchangeRateObj[0].rate;
+
+      logger.info('EUR to INR exchange rate:'+exchangeRate);
+      return exchangeRate;
+    } else {
+      logger.error('Failed to find exchange rate on the page.');
+      return null;
+    }
+  } catch (error) {
+    logger.error('Error fetching exchange rate:', error);
+    return null;
+  }
+}
+
+  // Set up a recurring job to fetch exchange rates every minute
+const interval = setInterval(() => {
+  logger.info('Currency Tracker Bot started....');
+  (async () => {
+    await processSendMessage();
+  })();
+}, config.get('Application.envConfig.set_timeout'));
